276bd189b4092b57adcc07a6acfa4e304f3f8726
Ghasem Kiani <ghasemkiani@gmail.com>
2024-08-31 21:36:36 +0330

    update

--- futil.js
+++ futil.js
@@ -1,48 +1,48 @@
 import path from "node:path";
 import fs from "node:fs";
 
-import {Obj} from "@ghasemkiani/base";
+import { Obj } from "@ghasemkiani/base";
 
 class FUtil extends Obj {
-	async toCopyDir(dir1, dir2) {
-		if(fs.existsSync(dir1)) {
-			await fs.promises.mkdir(dir2, {recursive: true});
-			let ff = await fs.promises.readdir(dir1, {withFileTypes: true});
-			for(let f of ff) {
-				let p1 = path.join(dir1, f.name);
-				let p2 = path.join(dir2, f.name);
-				if(f.isFile()) {
-					await fs.promises.copyFile(p1, p2);
-				} else if(f.isDirectory()) {
-					await this.toCopyDir(p1, p2);
-				}
-			}
-		}
-	}
-	async toDelDir(dir) {
-		if(fs.existsSync(dir)) {
-			let ff = fs.readdirSync(dir, {withFileTypes: true});
-			for(let f of ff) {
-				let p = path.join(dir, f.name);
-				if(f.isFile() || f.isSymbolicLink()) {
-					fs.unlinkSync(p);
-				} else if(f.isDirectory()) {
-					await this.toDelDir(p);
-				} else {
-					// ?
-				}
-			}
-			fs.rmdirSync(dir);
-		}
-	}
-	renameWithoutChangingLastModifiedDate(path1, path2) {
-		let {atime, mtime} = fs.statSync(path1, {});
-		atime = new Date();
-		fs.renameSync(path1, path2);
-		fs.utimesSync(path2, atime, mtime);
-	}
+  async toCopyDir(dir1, dir2) {
+    if (fs.existsSync(dir1)) {
+      await fs.promises.mkdir(dir2, { recursive: true });
+      let ff = await fs.promises.readdir(dir1, { withFileTypes: true });
+      for (let f of ff) {
+        let p1 = path.join(dir1, f.name);
+        let p2 = path.join(dir2, f.name);
+        if (f.isFile()) {
+          await fs.promises.copyFile(p1, p2);
+        } else if (f.isDirectory()) {
+          await this.toCopyDir(p1, p2);
+        }
+      }
+    }
+  }
+  async toDelDir(dir) {
+    if (fs.existsSync(dir)) {
+      let ff = fs.readdirSync(dir, { withFileTypes: true });
+      for (let f of ff) {
+        let p = path.join(dir, f.name);
+        if (f.isFile() || f.isSymbolicLink()) {
+          fs.unlinkSync(p);
+        } else if (f.isDirectory()) {
+          await this.toDelDir(p);
+        } else {
+          // ?
+        }
+      }
+      fs.rmdirSync(dir);
+    }
+  }
+  renameWithoutChangingLastModifiedDate(path1, path2) {
+    let { atime, mtime } = fs.statSync(path1, {});
+    atime = new Date();
+    fs.renameSync(path1, path2);
+    fs.utimesSync(path2, atime, mtime);
+  }
 }
 
 let futil = new FUtil();
 
-export {FUtil, futil};
+export { FUtil, futil };
--- inputter.js
+++ inputter.js
@@ -1,86 +1,90 @@
 import readline from "node:readline";
 
-import {cutil} from "@ghasemkiani/base";
-import {Obj} from "@ghasemkiani/base";
+import { cutil } from "@ghasemkiani/base";
+import { Obj } from "@ghasemkiani/base";
 
 class Inputter extends Obj {
-	// _rl
+  // _rl
 
-	get rl() {
-		if (!this._rl) {
-			this._rl = readline.createInterface({
-					input: process.stdin,
-					output: process.stdout,
-				});
-		}
-		return this._rl;
-	}
-	set rl(rl) {
-		this._rl = rl;
-	}
-	close() {
-		if (this._rl) {
-			this._rl.close();
-		}
-	}
-	async toReadLine(prompt) {
-		return new Promise((resolve, reject) => {
-			try {
-				if (typeof prompt === "undefined" || prompt === null) {
-					prompt = "";
-				} else if (typeof prompt !== "string") {
-					prompt = String(prompt);
-				}
-				this.rl.question(prompt, answer => {
-					resolve(answer);
-				});
-			} catch (e) {
-				reject(e);
-			}
-		});
-	}
-	async toReadLineTimeout(ms, prompt, defaultAnswer = null) {
-		return new Promise((resolve, reject) => {
-			try {
-				let rl = readline.createInterface({
-					input: process.stdin,
-					output: process.stdout,
-				});
-				let handle = setTimeout(() => {
-					rl.close();
-					resolve(defaultAnswer);
-				}, ms);
-				rl.question(cutil.asString(prompt), answer => {
-					clearTimeout(handle);
-					rl.close();
-					resolve(answer);
-				});
-			} catch(e) {
-				reject(e);
-			}
-		});
-	}
-	static async toInput(prompt) {
-		let inputter = new Inputter();
-		let line = await inputter.toReadLine(!prompt ? "" : prompt + " ");
-		inputter.close();
-		return line;
-	}
-	static async toInputTimeout(ms, prompt, defaultAnswer = null) {
-		let inputter = new Inputter();
-		let line = await inputter.toReadLineTimeout(ms, !prompt ? "" : prompt + " ", defaultAnswer);
-		inputter.close();
-		return line;
-	}
-	static async toConfirm(prompt) {
-		let line = await this.toInput(prompt);
-		return /^\s*y/i.test(line);
-	}
-	static async toConfirmOrThrow(prompt, message = "Aborted by user") {
-		if (!(await this.toConfirm(prompt))) {
-			throw new Error(message);
-		}
-	}
+  get rl() {
+    if (!this._rl) {
+      this._rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+    }
+    return this._rl;
+  }
+  set rl(rl) {
+    this._rl = rl;
+  }
+  close() {
+    if (this._rl) {
+      this._rl.close();
+    }
+  }
+  async toReadLine(prompt) {
+    return new Promise((resolve, reject) => {
+      try {
+        if (typeof prompt === "undefined" || prompt === null) {
+          prompt = "";
+        } else if (typeof prompt !== "string") {
+          prompt = String(prompt);
+        }
+        this.rl.question(prompt, (answer) => {
+          resolve(answer);
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+  async toReadLineTimeout(ms, prompt, defaultAnswer = null) {
+    return new Promise((resolve, reject) => {
+      try {
+        let rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout,
+        });
+        let handle = setTimeout(() => {
+          rl.close();
+          resolve(defaultAnswer);
+        }, ms);
+        rl.question(cutil.asString(prompt), (answer) => {
+          clearTimeout(handle);
+          rl.close();
+          resolve(answer);
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+  static async toInput(prompt) {
+    let inputter = new Inputter();
+    let line = await inputter.toReadLine(!prompt ? "" : prompt + " ");
+    inputter.close();
+    return line;
+  }
+  static async toInputTimeout(ms, prompt, defaultAnswer = null) {
+    let inputter = new Inputter();
+    let line = await inputter.toReadLineTimeout(
+      ms,
+      !prompt ? "" : prompt + " ",
+      defaultAnswer
+    );
+    inputter.close();
+    return line;
+  }
+  static async toConfirm(prompt) {
+    let line = await this.toInput(prompt);
+    return /^\s*y/i.test(line);
+  }
+  static async toConfirmOrThrow(prompt, message = "Aborted by user") {
+    if (!(await this.toConfirm(prompt))) {
+      throw new Error(message);
+    }
+  }
 }
 
-export {Inputter};
+export { Inputter };
